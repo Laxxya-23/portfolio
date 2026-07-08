384a7efb9a32c9f4a4cb13c97c7dcc97e0493e6f
Laxmi Narayan Jatav <laxminarayanjatavbtech2023@rietjaipur.ac.in>
2026-07-08 14:11:46 +0530

    Use Gmail SMTP TLS

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -2,18 +2,23 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
+    requireTLS: true,
+
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+
+    tls: {
+        rejectUnauthorized: false
+    }
 });
 
 const sendMail = async (name, email, subject, message) => {
 
     await transporter.sendMail({
-
         from: `"Portfolio Website" <${process.env.EMAIL_USER}>`,
         to: process.env.EMAIL_USER,
 
@@ -22,9 +27,9 @@ const sendMail = async (name, email, subject, message) => {
         html: `
         <h2>New Contact Form Submission</h2>
 
-        <p><b>Name:</b> ${name}</p>
-        <p><b>Email:</b> ${email}</p>
-        <p><b>Subject:</b> ${subject}</p>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Subject:</strong> ${subject}</p>
 
         <hr>
 
